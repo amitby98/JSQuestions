659d112553d07e0c30d429f90b76fbb7153cdc53
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-18 13:34:04 +0200

    Question 1 and 2

---
+++ Question1.js
@@ -0,0 +1,16 @@
+//The program accept two integers and display the larger
+
+let number1;
+let number2;
+
+number1 = prompt("Enter the first number");
+number2 = prompt("Enter the second number");
+
+number1 = Number(number1) 
+number2 = Number(number2)
+
+if (number1 > number2) {
+    alert('The larger number is' + number1);
+} else {
+    alert('The larger number is' + number2);
+} 
---
+++ Question2.js
@@ -0,0 +1,12 @@
+//JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
+
+let a = 3;
+let b = -7;
+let c = 2;
+let d = a*b*c;
+
+if (d>0) {
+    alert ("The sign is +");
+} else {
+    alert ("The sign is -");
+}
