7f73216d023f42b94ce8aa1e7e57ba8eab367206
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-18 15:12:46 +0200

    Question solution 5

---
+++ Question5.js
@@ -0,0 +1,9 @@
+//loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+for (let a = 0; a <= 15; a++) {
+    if (a % 2 === 0) {
+        console.log(a + "is even")
+    } else { 
+        console.log(a + "is odd")
+    }
+}
