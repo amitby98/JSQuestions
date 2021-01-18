eff6aa4f81ded61549e447b41677c568c1f09685
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-19 01:49:24 +0200

    Question solution 9

---
+++ Question9.js
@@ -0,0 +1,10 @@
+// find the armstrong numbers of 3 digits.
+for (let i = 100; i<1000; i++) {
+    let strNum = String(i);
+    let a = Number(strNum[0]);
+    let b = Number(strNum[1]);
+    let c = Number(strNum[2]);
+    if ((a**3) + (b**3) + (c**3) === i) {
+        console.log(i + "is the armstrong number");
+    }
+}
