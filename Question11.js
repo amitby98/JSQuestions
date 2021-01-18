af39b656c473ba9f7a7850e41470a866439737b1
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-19 01:27:03 +0200

    Question solution 11

---
+++ Question11.js
@@ -0,0 +1,19 @@
+//compute the greatest common divisor (GCD) of two positive integers.
+let a = 50;
+let b = 28;
+let gcd;
+while (a!=b)
+{
+	if (a>b)
+	{
+		gcd = b;
+	} else {
+	    gcd = a;
+	}
+}
+ for (gcd; gcd>0; gcd--) {
+     if (a%gcd===0 && b%gcd===0){
+         console.log("The number is" + gcd);
+         break;
+     }
+ }
