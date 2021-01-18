701239c453732013b1c14a7a75aee362432336fc
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-19 01:32:57 +0200

    Question solution 12

---
+++ Question12.js
@@ -0,0 +1,11 @@
+// sum the multiples of 3 and 5 under 1000.
+
+let sum = 0;
+for (let a = 0; a < 1000; a++)
+{
+    if (a % 3 === 0 || a % 5 === 0)
+    {
+       sum += a;
+    }
+}
+console.log(sum);
